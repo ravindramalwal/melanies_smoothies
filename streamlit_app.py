5921ea20d05bfdcb7b8fa19655e7175e0699c83a
ravindramalwal <35575123+ravindramalwal@users.noreply.github.com>
2025-03-02 17:53:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col ('FRUI
 #st.stop()
 # Convert the snowpark Dataframe to pandas Datafram sp we can use the LOc Function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
+#st.dataframe(pd_df)
 
 
 ingredients_list = st.multiselect(
@@ -37,6 +37,8 @@ if    ingredients_list:
       
       for fruit_chosen in ingredients_list:
           ingredients_string += fruit_chosen + ''
+          search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+          st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
           st.subheader(fruit_chosen + 'Nutrition Information')
           smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
           sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
