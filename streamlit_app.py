934eea706730e15ed1c63181046c324e9c6deaee
ravindramalwal <35575123+ravindramalwal@users.noreply.github.com>
2025-03-03 10:03:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col ('FRUIT_NAME'), col('SEARCH_ON')) 
 
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 # Convert the snowpark Dataframe to pandas Datafram sp we can use the LOc Function
 pd_df=my_dataframe.to_pandas()
